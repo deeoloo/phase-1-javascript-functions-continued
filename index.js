98be2accf340c2ae8d25fda1ee01d2c03fb58308
deenoloo <doreenoloo2@gmail.com>
2025-03-05 03:58:06 -0500

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun (activity=`roller-skate`){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+function mondayWork(todo=`go to the office`){
+    return `This Monday, I will ${todo}.`;
+}
+function wrapAdjective(style=`*`){
+    return function(fault=`special`){
+    return  `You are ${style}${fault}${style}!`;
+    }
+}
